2745c57996192904a5f4c9afe530432eaaf3b8d5
akki2488 <akshaymishra2488@gmail.com>
2018-11-16 00:55:21 +0530

    product categories added

---
+++ product-catalog-api/src/main/java/com/bss/product/catalog/model/entities/ProductCategory.java
@@ -0,0 +1,36 @@
+package com.bss.product.catalog.model.entities;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.Set;
+
+/**
+ * Created by Rocky on 16-11-2018.
+ */
+@Document(collection="productCategories")
+
+public class ProductCategory {
+
+    @Id
+    private String id;
+
+    @NotBlank
+    @Size(max=100)
+    @Indexed(unique=true)
+    private String name;
+
+    @Size(max=1000)
+    private String description;
+
+    @DBRef
+    private Set<Product> products;
+
+    private Integer displayOrder;
+
+    private boolean disabled;
+}
